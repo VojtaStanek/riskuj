d4de079d1503fdaeb798578ae1331b41251f9086
Vojta Staněk <stanekv01@gmail.com>
2019-01-18 14:11:52 +0100

    ☑

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,239 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { QUESTIONS } from './questions'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+type TeamId = string
+
+type GroupId = string
+
+// type
+type GridTo<T> = { [col in GroupId]: { [row: number]: T } }
+
+interface GameState {
+	points: { [_ in TeamId]: number }
+	questions: GridTo<TeamId | undefined>
+	teamOnTurn: TeamId
 }
 
-export default App;
+interface AppState {
+	gameState: GameState
+	openQuestion: [GroupId, number] | undefined
+	openedCorrect: Correctness | undefined
+}
+
+export interface Question {
+	group: GroupId
+	row: number
+	question: string
+}
+
+// const QUESTIONS: Question[] = []
+
+// reduce(function (r, a) {
+//   r[a.make] = r[a.make] || [];
+//   r[a.make].push(a);
+//   return r;
+// }, Object.create(null));
+
+const GROUPS = QUESTIONS.map(t => t.group).reduce<GroupId[]>(
+	(acc, v) => (acc.includes(v) ? acc : [...acc, v]),
+	[]
+)
+const ROWS = QUESTIONS.map(t => t.row)
+	.reduce<number[]>((acc, v) => (acc.includes(v) ? acc : [...acc, v]), [])
+	.sort()
+
+const TEAMS: TeamId[] = ['A', 'B', 'C', 'D']
+const TEAMS_COUNT = TEAMS.length
+
+enum Correctness {
+	Correct = 'Correct',
+	Wrong = 'Wrong'
+}
+
+const nextTeam = (team: TeamId) =>
+	TEAMS[(TEAMS.indexOf(team) + 1) % TEAMS.length]
+
+class App extends Component<{}, AppState> {
+	state: AppState = {
+		gameState: {
+			points: {
+				...TEAMS.reduce((a, t) => ({ ...a, [t]: 0 }), {})
+			},
+			questions: {},
+			teamOnTurn: TEAMS[0]
+		},
+		openQuestion: undefined,
+		openedCorrect: undefined
+	}
+
+	componentDidMount() {
+		document.addEventListener('keydown', event => {
+			let correctMultiplier = 0
+			let correctness = Correctness.Wrong
+			switch (event.key) {
+				case 'c':
+					correctMultiplier = 1
+					correctness = Correctness.Correct
+				case 'w':
+					this.setState(state => {
+						if (!state.openQuestion) return state
+						return {
+							...state,
+							openedCorrect: correctness,
+							// openQuestion: undefined,
+							gameState: {
+								...state.gameState,
+								teamOnTurn: nextTeam(state.gameState.teamOnTurn),
+								points: {
+									...state.gameState.points,
+									[state.gameState.teamOnTurn]:
+										state.gameState.points[state.gameState.teamOnTurn] +
+										(state.openQuestion as [unknown, number])[1] *
+											correctMultiplier
+								},
+								questions: {
+									...state.gameState.questions,
+									[(state.openQuestion as [GroupId, number])[0]]: {
+										...(state.gameState.questions[
+											(state.openQuestion as [GroupId, number])[0]
+										] || {}),
+										[(state.openQuestion as [GroupId, number])[1]]:
+											state.gameState.teamOnTurn
+									}
+								}
+							}
+						}
+					})
+					break
+				case 'x':
+				case 'p':
+					this.setState(state => ({
+						...state,
+						openQuestion: undefined,
+						openedCorrect: undefined
+					}))
+					break
+				case 'n':
+					this.setState(state => ({
+						...state,
+						openQuestion: undefined,
+						gameState: {
+							...state.gameState,
+							teamOnTurn: nextTeam(state.gameState.teamOnTurn)
+						}
+					}))
+					break
+			}
+		})
+	}
+
+	renderQuestion(question: Question): React.ReactNode {
+		const questions = this.state.gameState.questions
+		const q =
+			questions &&
+			questions[question.group] &&
+			questions[question.group][question.row]
+		return (
+			<div
+				className="cell"
+				style={
+					{
+						'--row': ROWS.indexOf(question.row),
+						'--group': GROUPS.indexOf(question.group)
+					} as any
+				}
+				onClick={() => {
+					this.setState(state => ({
+						...state,
+						openQuestion: [question.group, question.row]
+					}))
+				}}
+			>
+				<div
+					className={`cell-inner ${
+						q ? `answered ofTeam${TEAMS.indexOf(q)}` : `open`
+					}`}
+				>
+					{question.row}
+				</div>
+			</div>
+		)
+	}
+
+	render() {
+		const { gameState, openQuestion, openedCorrect } = this.state
+		// const groups
+		const groupedQuestions = QUESTIONS.reduce<GridTo<Question>>(
+			(r, v) => ({
+				...r,
+				[v.group]: { ...(r[v.group] || {}), [v.row]: v }
+			}),
+			{}
+		)
+
+		const allAnswered =
+			GROUPS.every(g => typeof gameState.questions[g] !== 'undefined') &&
+			Object.values(gameState.questions).every(g =>
+				ROWS.every(r => g && Object.keys(g).includes(r.toString()))
+			)
+		return (
+			<div className="App">
+				{openQuestion && (
+					<div
+						className={`openedQuestion is${
+							openedCorrect ? openedCorrect : 'None'
+						}`}
+					>
+						<div className="openedQuestion-content">
+							<div className="openedQuestion-label">
+								{openQuestion[0]} za {openQuestion[1]}
+							</div>
+							<div className="openedQuestion-question">
+								{groupedQuestions[openQuestion[0]][openQuestion[1]].question}
+							</div>
+						</div>
+					</div>
+				)}
+				<div className="home">
+					<div className="points">
+						{Object.entries(gameState.points).map(([team, points]) => (
+							<div
+								className={`points-team ${
+									gameState.teamOnTurn == team ? 'onTurn' : ''
+								}`}
+							>
+								TÝM {team}: ${points}
+							</div>
+						))}
+					</div>
+					<div className="grid-outer">
+						<div
+							className="grid"
+							style={
+								{ '--groups': GROUPS.length, '--rows': ROWS.length } as any
+							}
+						>
+							<div className="groups">
+								{GROUPS.map((group, i) => (
+									<div className="group" style={{ '--group': i } as any}>
+										<div className="cell-inner">{group}</div>
+									</div>
+								))}
+							</div>
+							{Object.entries(groupedQuestions).map(([group, column]) => (
+								<div className="column">
+									{Object.entries(column).map(([row, question]) =>
+										this.renderQuestion(question)
+									)}
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ src/questions.tsx
@@ -0,0 +1,142 @@
+import { Question } from './App'
+
+export const QUESTIONS: Question[] = [
+	{
+		group: 'Prvotní doklady',
+		row: 1000,
+		question:
+			'Prvotní doklady často dostáváme vytištěné z registrační poklady, přesto se někdy může stát, že je pro nás vypisován rukou. Jakou náležitost navíc očekáváme na takovémto dokladu?'
+	},
+	{
+		group: 'Prvotní doklady',
+		row: 2000,
+		question:
+			'Jaké dvě náležitosti vyžadujeme na tisknutém prvotním dokladu a zde chybí? Identifikace prodejce, obsah transakce.'
+	},
+	{
+		group: 'Prvotní doklady',
+		row: 3000,
+		question:
+			'Na prvotních dokladech se uvádí identifikace prodejce. Jaké tři údaje by měla tato identifikace pro naše účely obsahovat?'
+	},
+	{
+		group: 'Prvotní doklady',
+		row: 4000,
+		question:
+			'Na prvotních dokladech by měl být vypsán obsah transakce. Jaké údaje očekáváme u každé položky?'
+	},
+	{
+		group: 'Prvotní doklady',
+		row: 5000,
+		question:
+			'Pokud se na akci používá vozidlo, které není majetkem střediska musíme se řídit směrnicí o proplácení cestovních náhrad. Tento předpis stanovuje, jakou formou má být počítána proplácená částka. Jak se jmenuje formulář, který je třeba vyplnit a který poté považujeme za prvotní doklad?'
+	},
+	{ group: 'Řád', row: 1000, question: 'Jak vypadá rozpočet akce?' },
+	{
+		group: 'Řád',
+		row: 2000,
+		question: 'Které jednotky v junáku mají právní osobnost (subjektivitu)?'
+	},
+	{
+		group: 'Řád',
+		row: 3000,
+		question: 'Jaký je rozdíl mezi hospodařením a účetnictvím?'
+	},
+	{
+		group: 'Řád',
+		row: 4000,
+		question: 'Kdo podle řádu nese hlavní odpovědnost za hospodaření na akcích?'
+	},
+	{
+		group: 'Řád',
+		row: 5000,
+		question:
+			'Akce rozlišujeme na malé velké. Jaká jsou stanoveny hranice, pod kterými je akce malá?'
+	},
+	{
+		group: 'Příprava a vyúčtování výpravy',
+		row: 1000,
+		question:
+			'V pokladní knize a na pokladním dokladu je sloupec „Zůstatek“. Co v něm uvádíme?'
+	},
+	{
+		group: 'Příprava a vyúčtování výpravy',
+		row: 2000,
+		question:
+			'Pokud při přípravě rozpočtu neznáme finální cenu, jak se zachováme?'
+	},
+	{
+		group: 'Příprava a vyúčtování výpravy',
+		row: 3000,
+		question: 'Jakými dvěma (až třemi) způsoby může finančně skončit akce?'
+	},
+	{
+		group: 'Příprava a vyúčtování výpravy',
+		row: 4000,
+		question:
+			'V pokladní knize a na pokladním dokladu je sloupec „Datum“. Co v něm uvádíme?'
+	},
+	{
+		group: 'Příprava a vyúčtování výpravy',
+		row: 5000,
+		question:
+			'V rozpočtu se na straně výdajů objevuje položka „rezerva“. O co jde, proč ji tam zařazujeme a v jaké výši ji stanovujeme.'
+	},
+	{
+		group: 'Pokladní doklad',
+		row: 1000,
+		question: 'Jaké základní dva druhy pokladních dokladů rozlišujeme?'
+	},
+	{
+		group: 'Pokladní doklad',
+		row: 2000,
+		question: 'Kdo bude kde podepsán u následujícího dokladu?'
+	},
+	{
+		group: 'Pokladní doklad',
+		row: 3000,
+		question:
+			'Kolik prvotních dokladů může patřit k jednomu pokladnímu dokladu a kolik pokladních k jednomu prvotnímu?'
+	},
+	{
+		group: 'Pokladní doklad',
+		row: 4000,
+		question:
+			'Co je to převod mezi pokladnami a jaký má dopad na celkový finanční výsledek akce?'
+	},
+	{
+		group: 'Pokladní doklad',
+		row: 5000,
+		question:
+			'Komu typicky dáváme zálohu a jak s ní budeme pracovat v pokladní knize?'
+	},
+	{
+		group: 'Junák v právu a jeho majetek',
+		row: 1000,
+		question: 'Jak obvykle vypadají názvy středisek jako právnických osob?'
+	},
+	{
+		group: 'Junák v právu a jeho majetek',
+		row: 2000,
+		question:
+			'Vyjmenuj alespoň dva základní vnitřní předpisy upravující hospodaření.'
+	},
+	{
+		group: 'Junák v právu a jeho majetek',
+		row: 3000,
+		question:
+			'Spolek, což je právní forma Junáka, se považuje za neziskovou organizaci. Co je touto „neziskovostí“ myšleno?'
+	},
+	{
+		group: 'Junák v právu a jeho majetek',
+		row: 4000,
+		question:
+			'Odkud můžeme čerpat finance pro naši činnost. Vyjmenuj alespoň 4.'
+	},
+	{
+		group: 'Junák v právu a jeho majetek',
+		row: 5000,
+		question:
+			'Pro naši činnost potřebuje určitý majetek. Co musíme dělat s majetkem každý rok a při jeho změnách (nabytí, zcizení)?'
+	}
+]
